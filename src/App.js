bb578d6f9c1923beda1d76814cedfbf5d5951cb3
tamim <tamimarefinanik@gmail.com>
2024-02-08 16:57:56 +0600

    adding json server

--- src/App.js
+++ src/App.js
@@ -9,38 +9,7 @@ function App() {
 
   const [showForm, setShowForm] = useState(false)
 
-  const [tasks, setTasks] = useState([
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2.30 pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Grocery Shopping',
-        day: 'Feb 6th at 10.00 am',
-        reminder: false
-    },
-    {
-        id: 3,
-        text: 'Meeting with Client',
-        day: 'Feb 7th at 3.00 pm',
-        reminder: true
-    },
-    {
-        id: 4,
-        text: 'Gym Session',
-        day: 'Feb 8th at 8.00 am',
-        reminder: false
-    },
-    {
-        id: 5,
-        text: 'Dentist Appointment',
-        day: 'Feb 9th at 11.30 am',
-        reminder: true
-    }    
-  ])
+  const [tasks, setTasks] = useState([])
 
   const onToggleTask = (id) => {
     setTasks(tasks.map((t) => 
